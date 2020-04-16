2d2947e25617089ef66d66fef2d5b1cd28f3f022
Daniel Partadzhiev <daniel.partadzhiev@gmail.com>
2020-04-16 12:09:42 +0300

    Third commit

--- src/helpers/addUser.js
+++ src/helpers/addUser.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 export const addUser = ({ username, password, role }) => {
   const users = JSON.parse(window.localStorage.getItem("users"));
   const updatedUsers = JSON.stringify([
-    { id: uuidv4(), username, password, role },
+    { id: uuidv4(), username, password, role, tasks: []},
     ...users,
   ]);
   window.localStorage.setItem("users", updatedUsers);
